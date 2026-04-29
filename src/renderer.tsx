1b71a9b618285508176a5b1f92203746c1a7ad06
제미제미 <jjemi4067@gmail.com>
2026-04-29 15:07:12 +0000

    feat: Google Search Console 소유권 확인 메타태그 추가

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -43,6 +43,8 @@ export const renderer = jsxRenderer(({ children, title, description, ogImage, js
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.0/css/all.min.css" />
         {/* Custom CSS */}
         <link rel="stylesheet" href="/static/style.css" />
+        {/* Google Search Console 소유권 확인 */}
+        <meta name="google-site-verification" content="a57r9VKSo4eR77ebK5nrIlr0-mCJYnpZSn-Oovxrte4" />
         {/* Google AdSense */}
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5237218812452624" crossorigin="anonymous"></script>
         <script dangerouslySetInnerHTML={{__html: `
